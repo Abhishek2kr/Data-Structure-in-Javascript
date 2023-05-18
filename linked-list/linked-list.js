cab6b01d5a063a622d94a9d53dc4f79829a72576
Abhishek Kumar <abhishek2krs@gmail.com>
2023-05-18 16:43:16 +0530

    Linked-list -> push method added

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -11,8 +11,24 @@ class LinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    push (value) {
+       if(this.length === 0) {
+        this.head = new Node(value);
+        this.tail = this.head;
+       } else {
+        this.tail.next = new Node(value);
+        this.tail = this.tail.next;
+       }
+       this.length++;
+    }
 }
 
-// 
+// Init
 const list = new LinkedList();
+// Insert
+list.push(23);
+list.push(34);
+list.push(45);
+// Display
 console.log(list);
