53af11bfc9e8530b3e163d7b8181de5d298c9d45
Abhishek Kumar <abhishek2krs@gmail.com>
2023-05-26 23:08:20 +0530

    Binary-search-tree -> BFS and DFS added

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -27,6 +27,38 @@ class BinarySearchTree {
         else if(prev.data > data) prev.left = node;
         else prev.right = node;
     }
+
+    bfs() {
+        const data = [];
+        const queue = [];
+        if(!this.root) return data;
+        queue.push(this.root);
+        while(queue.length) {
+            const node = queue.pop();
+            data.push(node.data);
+            if(node.left) queue.unshift(node.left);
+            if(node.right) queue.unshift(node.right);
+        }
+        return data;
+    }
+    // using recursion
+    dfsPreOrder(root) {
+       if(!root) return;
+       console.log(root.data);
+       this.dfsPreOrder(root.left);
+       this.dfsPreOrder(root.right);
+    }
+    // using stack
+    dfsPreOrderUsingStack() {
+        const stack = [];
+        stack.push(this.root);
+        while(stack.length) {
+            const node = stack.pop();
+            console.log(node.data);
+            if(node.right) stack.push(node.right);
+            if(node.left) stack.push(node.left);
+        }
+     }
 }
 
 const BST = new BinarySearchTree();
@@ -38,3 +70,8 @@ BST.insert(14);
 BST.insert(19);
 
 console.log(BST.root);
+console.log(BST.bfs());
+
+// DFS PreOrder
+BST.dfsPreOrder(BST.root);
+BST.dfsPreOrderUsingStack();
