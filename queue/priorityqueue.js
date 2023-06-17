f79ceecd9e2e8fc91912bedb71efc5d6a7c08e37
Abhishek Kumar <abhishek2krs@gmail.com>
2023-06-17 23:58:45 +0530

    Priority-Queue -> Max Priority Queue added

--- queue/priorityqueue.js
+++ queue/priorityqueue.js
@@ -20,35 +20,35 @@ class MaxPriorityQueue {
         }
     }
 
-    dequeue(){
-        if(!this.length) {
+    dequeue() {
+        if (!this.length) {
             return undefined;
         }
         const popped = this.queue[0];
         this.queue[0] = this.queue[this.length - 1];
         this.queue[this.length - 1] = popped;
         this.length--;
-        let i  = 1;
-        while( i < this.length) {
-          let left = i * 2;
-          let right = i * 2 + 1;
-          let swapIndex;
-          const element = this.queue[i-1];
-          if(left <= this.length) {
-            if( element[1] < this.queue[left-1][1]) {
-              swapIndex = left;
+        let i = 1;
+        while (i < this.length) {
+            let left = i * 2;
+            let right = i * 2 + 1;
+            let swapIndex;
+            const element = this.queue[i - 1];
+            if (left <= this.length) {
+                if (element[1] < this.queue[left - 1][1]) {
+                    swapIndex = left;
+                }
             }
-          }
-          if(right <= this.length) {
-            if((swapIndex != null && this.queue[left - 1][1] < this.queue[right-1][1])|| 
-                (swapIndex == null && element[1] < this.queue[right-1][1])) {
-              swapIndex = right;
+            if (right <= this.length) {
+                if ((swapIndex != null && this.queue[left - 1][1] < this.queue[right - 1][1]) ||
+                    (swapIndex == null && element[1] < this.queue[right - 1][1])) {
+                    swapIndex = right;
+                }
             }
-          }
-          if(swapIndex == null) return popped;
-          this.queue[i-1] = this.queue[swapIndex - 1];
-          this.queue[swapIndex -1] = element;
-          i = swapIndex;
+            if (swapIndex == null) return popped;
+            this.queue[i - 1] = this.queue[swapIndex - 1];
+            this.queue[swapIndex - 1] = element;
+            i = swapIndex;
         }
         return popped;
     }
